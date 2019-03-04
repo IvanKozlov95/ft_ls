a4acd966dcb4c8fda7d7d526c0224dd4e279269e
Ivan Kozlov <ikozlov@student.42.us.org>
2019-03-04 02:25:07 -0800

    Upd header

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 17:31:17 by ikozlov           #+#    #+#             */
-/*   Updated: 2019/03/02 21:36:35 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/03/04 02:00:43 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,13 @@ typedef struct s_ls		t_ls;
 # define FLAG_T 0x10
 
 # define SUPPORTED_FLAGS "alrRt"
-# define FLAGS_VALUES [FLAG_A, FLAG_L, FLAG_R, FLAG_RR, FLAG_T]
 
 t_ls	*ls_init(void);
 int		get_flag_value(char f);
 
-int		parse(int ac, const char *av[], t_ls *ls);
+int				parse(int ac, const char *av[], t_ls *ls);
 
-void	ft_ls_usage(void);
+void			ft_ls_usage(void);
+void			print_ls(t_ls *ls);
 
 #endif
