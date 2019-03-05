d2445ad378d8c985aba29db53477ed9a95a518f5
Ivan Kozlov <ikozlov@student.42.us.org>
2019-03-05 02:09:05 -0800

    Upd header

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 17:31:17 by ikozlov           #+#    #+#             */
-/*   Updated: 2019/03/04 03:18:54 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/03/04 20:40:56 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 # define FT_LS_H
 
 # include "list.h"
+# include "ft_printf.h"
+
+# include <stdlib.h>
+# include <dirent.h>
 # include <sys/stat.h>
 
 struct					s_ls
@@ -23,12 +27,23 @@ struct					s_ls
 };
 typedef struct s_ls		t_ls;
 
+struct					s_dir
+{
+	char				*path;
+
+	DIR					*dir;
+	struct dirent		*file;
+};
+typedef struct s_dir	t_dir;
+
 struct					s_arg
 {
 	char			*path;
-	struct stat		stat;
-
+	char			*name;
+	char			is_hidden;
 	unsigned char	not_found;
+
+	struct stat		stat;
 };
 typedef struct s_arg	t_arg;
 
@@ -44,16 +59,28 @@ typedef struct s_arg	t_arg;
 
 # define SUPPORTED_FLAGS "alrRt"
 
+# define CURR_DIR "."
+# define PARENT_DIR ".."
+
 t_ls			*ls_init(void);
-t_arg			*arg_init(char *path);
+t_arg			*arg_init(const char *name);
 void			arg_destroy(t_arg *arg);
 int				get_flag_value(char f);
 
 int				parse(int ac, const char *av[], t_ls *ls);
 
 void			ls_process(t_ls *ls);
+void			get_arg_info(t_list *node);
 
 void			ft_ls_usage(void);
 void			print_ls(t_ls *ls);
+t_list			*get_dir_content(t_dir dir);
+
+void			display_dirs(t_list *node);
+void			process_dirs(t_list *args);
+
+void			display_files(t_list *node);
+
+char			*build_path(char *dir, char *file_name);
 
 #endif
